ec9eca5d649e0e2b5eb39d7710bb37801f146be5
Paras Toor <parasindersingh@gmail.com>
2025-05-24 22:50:01 +0530

    add pagination on the frontend

--- apps/frontend/src/app/home/home.component.ts
+++ apps/frontend/src/app/home/home.component.ts
@@ -4,6 +4,7 @@ import { CardComponent } from '../card/card.component';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
+  standalone: true,
   selector: 'home',
   imports: [CardComponent, CommonModule],
   templateUrl: './home.component.html',
@@ -11,18 +12,46 @@ import { HttpClient } from '@angular/common/http';
 })
 export class HomeComponent {
   compounds: any[] = [];
+  currentPage = 1;
+  totalPages = 1;
+  limit = 10;
 
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
-    this.http.get('http://localhost:4000/api/v1/compounds').subscribe({
-      next: (res: any) => {
-        this.compounds = res.data;
-        console.log('data in compounds  -', this.compounds);
-      },
-      error: (err) => {
-        console.error('API error:', err);
-      },
-    });
+    this.fetchCompounds(this.currentPage);
+  }
+
+  fetchCompounds(page: number) {
+    this.http
+      .get(
+        `http://localhost:4000/api/v1/compounds?pageNumber=${page}&pageSize=${this.limit}`,
+      )
+      .subscribe({
+        next: (res: any) => {
+          // const { data, currentPage, totalPages } = res;
+          // console.log('data   -', data);
+          // console.log('currentPage   -', currentPage);
+          // console.log('totalPages   -', totalPages);
+          this.compounds = res.data;
+          this.currentPage = +res.currentPage;
+          this.totalPages = +res.totalPages;
+        },
+        error: (err) => {
+          console.error('API error:', err);
+        },
+      });
+  }
+
+  nextPage() {
+    if (this.currentPage < this.totalPages) {
+      this.fetchCompounds(this.currentPage + 1);
+    }
+  }
+
+  prevPage() {
+    if (this.currentPage > 1) {
+      this.fetchCompounds(this.currentPage - 1);
+    }
   }
 }
